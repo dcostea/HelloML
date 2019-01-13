257d901667552a0e2657227e05435e55ad9c2822
Daniel Costea <daniel_costea@ymail.com>
2019-01-13 20:42:06 +0200

    Adding project files

---
+++ HelloML/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Data;
+using System;
+
+namespace HelloML
+{
+    class Program
+    {
+        private static MLContext Context { get; set; }
+        private static ITransformer Model { get; set; }
+
+        static void Main(string[] args)
+        {
+            const string sampleCsv = "sample.csv";
+
+            Context = new MLContext(seed: 0);
+
+            // get training data
+            var reader = Context.Data.CreateTextReader(new TextLoader.Arguments
+            {
+                Separator = ",",
+                HasHeader = false,
+                Column = new[]
+                {
+                    new TextLoader.Column("MinHour", DataKind.R4, 0),
+                    new TextLoader.Column("MaxHour", DataKind.R4, 1),
+                    new TextLoader.Column("Label", DataKind.TX, 2)
+                },
+            });
+            var trainingDataView = reader.Read(sampleCsv);
+
+            // train the model
+            var trainingPipeline = Context.Transforms.Concatenate("Features", "MinHour", "MaxHour")
+                .Append(Context.Transforms.Conversion.MapValueToKey("Label"), TransformerScope.Everything)
+                .Append(Context.MulticlassClassification.Trainers.LogisticRegression())
+                .Append(Context.Transforms.Conversion.MapKeyToValue(("PredictedLabel", "PredictedLabel")));
+
+            Model = trainingPipeline.Fit(trainingDataView);
+
+            // predict data
+            var sample1 = new SourceData
+            {
+                MinHour = 22,
+                MaxHour = 23
+            };
+            Console.WriteLine($"Predict greeting for interval {sample1.MinHour} - {sample1.MaxHour}:");
+            Console.WriteLine($"{Predict(sample1).PredictedLabel}");
+
+            var sample2 = new SourceData
+            {
+                MinHour = 9,
+                MaxHour = 11
+            };
+            Console.WriteLine($"Predict greeting for interval {sample2.MinHour} - {sample2.MaxHour}:");
+            Console.WriteLine($"{Predict(sample2).PredictedLabel}");
+
+            var sample3 = new SourceData
+            {
+                MinHour = 16,
+                MaxHour = 17
+            };
+            Console.WriteLine($"Predict greeting for interval {sample3.MinHour} - {sample3.MaxHour}:");
+            Console.WriteLine($"{Predict(sample3).PredictedLabel}");
+
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        static SourcePrediction Predict(SourceData data)
+        {
+            var predictionEngine = Model.CreatePredictionEngine<SourceData, SourcePrediction>(Context);
+            return predictionEngine.Predict(data);
+        }
+    }
+
+    public class SourceData
+    {
+        public float MinHour { get; set; }
+
+        public float MaxHour { get; set; }
+
+        [ColumnName("Label")]
+        public string Label;
+    }
+
+    class SourcePrediction
+    {
+        public string PredictedLabel { get; set; }
+    }
+}
