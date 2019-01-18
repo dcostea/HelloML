f313a046207885331474e0727c255858da9d92c9
Daniel Costea <daniel_costea@ymail.com>
2019-01-19 00:06:15 +0200

    Add Testing the Model

--- HelloML/Program.cs
+++ HelloML/Program.cs
@@ -14,7 +14,7 @@ namespace HelloML
         {
             const string sampleCsv = "sample.csv";
 
-            Context = new MLContext(seed: 0);
+            Context = new MLContext(seed: 1);
 
             // get training data
             var reader = Context.Data.CreateTextReader(new TextLoader.Arguments
@@ -28,22 +28,27 @@ namespace HelloML
                     new TextLoader.Column("Label", DataKind.TX, 2)
                 },
             });
-            WriteLine("Loading training data...");
-            var trainingDataView = reader.Read(sampleCsv);
+            WriteLine("Loading data...");
+            var data = reader.Read(sampleCsv);
+            var (trainingDataView, testingDataView) = Context.MulticlassClassification.TrainTestSplit(data, testFraction: 0.2, seed: 1);
 
             // train the model
             var trainingPipeline = Context.Transforms.Concatenate("Features", "MinHour", "MaxHour")
                 .Append(Context.Transforms.Conversion.MapValueToKey("Label"), TransformerScope.Everything)
-                .AppendCacheCheckpoint(Context)
-                //.Append(Context.MulticlassClassification.Trainers.LogisticRegression())
-                .Append(Context.MulticlassClassification.Trainers.StochasticDualCoordinateAscent())
+                .Append(Context.MulticlassClassification.Trainers.LogisticRegression())
+                //.AppendCacheCheckpoint(Context)
+                //.Append(Context.MulticlassClassification.Trainers.StochasticDualCoordinateAscent())
                 .Append(Context.Transforms.Conversion.MapKeyToValue(("PredictedLabel", "PredictedLabel")));
 
             WriteLine("Training the model...");
             Model = trainingPipeline.Fit(trainingDataView);
 
+            var metrics = Context.MulticlassClassification.Evaluate(Model.Transform(testingDataView), "Label", "Score");
+            PrintMultiClassClassificationMetrics("", metrics);
+
             // predict data
             WriteLine("Predict some data...");
+
             var sample1 = new SourceData
             {
                 MinHour = 22,
@@ -74,6 +79,20 @@ namespace HelloML
         {
             var predictionEngine = Model.CreatePredictionEngine<SourceData, SourcePrediction>(Context);
             return predictionEngine.Predict(data);
+        }
+
+        public static void PrintMultiClassClassificationMetrics(string name, MultiClassClassifierMetrics metrics)
+        {
+            WriteLine($"************************************************************");
+            WriteLine($"*    Metrics for {name} multi-class classification model   ");
+            WriteLine($"*-----------------------------------------------------------");
+            WriteLine($"    AccuracyMacro = {metrics.AccuracyMacro:0.####}, a value between 0 and 1, the closer to 1, the better");
+            WriteLine($"    AccuracyMicro = {metrics.AccuracyMicro:0.####}, a value between 0 and 1, the closer to 1, the better");
+            WriteLine($"    LogLoss = {metrics.LogLoss:0.####}, the closer to 0, the better");
+            WriteLine($"    LogLoss for class 1 = {metrics.PerClassLogLoss[0]:0.####}, the closer to 0, the better");
+            WriteLine($"    LogLoss for class 2 = {metrics.PerClassLogLoss[1]:0.####}, the closer to 0, the better");
+            WriteLine($"    LogLoss for class 3 = {metrics.PerClassLogLoss[2]:0.####}, the closer to 0, the better");
+            WriteLine($"************************************************************");
         }
     }
 
